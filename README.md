c25cfded91f4e5bc74b8bc5260afdf84fc7664e7
Meet Vardoriya <64339539+mv1249@users.noreply.github.com>
2021-07-14 09:30:19 +0530

    ScorePredictor
    Here in this project i have made an IPL First Innings Score Predictor,which predicts the estimated score in a range,of the First Innings based on Current Overs,Current Wickets Current Runs,apart from this i have also made the Dashboard which displays the visuals,of the top Performing Batsmen and Bowlers during the Season,in Indian Premier League,the Top performing Batsman is often given the orange cap and the top performing bowler is given the Purple Cap,so the ball by ball statistics has been made in the visual form in the Dashboards,namely the Orange Cap Dashboard,and the Purple Cap Dashboard,here are the set of frameworks which i used for this project

--- README.md
+++ README.md
@@ -1,82 +1,13 @@
-# Covid19Tracker
+# ScorePredictor
 
-Get all the latest updates and details regarding Covid-19 here at CovidTracker,we provide all the necessary information:)
-All the details,statistics regarding Covid-19 in India is been displayed at Covid-Tracker,just follow on with the readme file,you'll get it whole.
-Before getting into the necessary details,let us see the proposed methodology which was followed to make up Covid-Tracker😷
+Here in this project i have made an IPL First Innings Score Predictor,which predicts the estimated score in a range,of the First Innings based on Current Overs,Current Wickets
+Current Runs,apart from this i have also made the Dashboard which displays the visuals,of the top Performing Batsmen and Bowlers during the Season,in Indian Premier League,the 
+Top performing Batsman is often given the orange cap and the top performing bowler is given the Purple Cap,so the ball by ball statistics has been made in the visual form in the
+Dashboards,namely the Orange Cap Dashboard,and the Purple Cap Dashboard,here are the set of frameworks which i used for this project 
 
+Here is url from where you can access the project--> https://firstinningsscorepredictor.herokuapp.com/
 
-## Methodolgy Used
-
-
-<p align = "center">
-   <img src="https://github.com/mv1249/Covid19Tracker/blob/main/images/workflow.PNG">
-</p>
-
-As we can observe from the Methodology i have used Data Scraping and integrating that with the web servers,i have divided CovidTracker into several components,
-here is a preview of the components,
-
-[<img align = right height = 50 width = 50 src = "https://www.vhv.rs/dpng/d/451-4511009_cool-chrome-transparent-custom-google-chrome-logo-hd.png">](https://trackcovid19here.herokuapp.com/)
-
-[<img align = right height = 50 width = 50 src = "https://edent.github.io/SuperTinyIcons/images/svg/github.svg">](https://github.com/mv1249/Covid19Tracker)
-
-[<img align = right height = 50 width = 50 src = https://cdn4.iconfinder.com/data/icons/social-media-and-logos-11/32/Logo_Youtube-512.png>](https://github.com/mv1249/Covid19Tracker/issues/1#issue-919445508)
-
-
-
-# Demo
-
-
-
-
-## Quick Access Covid-19 Dashboard
-
-The user can get a quick access to the trends of Covid-19 accross India,all the Active,Confirmed,Recovered,Death Cases have been mentioned in a nutshell here,one can get a 
-complete overview of what is the situation of Covid-19 in India
-
-<p align = "center">
-   <img src="https://github.com/mv1249/Covid19Tracker/blob/main/images/First.gif" width = "975" height = "475">
-</p>
-
-
-## Quick Access To Covid-19 Vaccination
-The user can get a quick access to the trends of Covid-19 Vaccination accross India,how many states have given the first dose,how many have given the second dose and collectively
-what % of people are completly vaccinated in the state,the user can get all this information in a nutshell here at Covid-19 Vaccine Dashboard.
-
-<p align = "center">
-   <img src="https://github.com/mv1249/Covid19Tracker/blob/main/images/Second.gif" width = "975" height = "475">
-</p>
-
-## Covid-19 Case Dashboard
-
-Here the user has to enter the name of the state for which user is looking for,in the search bar and hit search,the user will be redirected to the Dashboard page,where the
-user can get the clear idea of what is the condition of the state which he is staying in with the top infected states accross india,at the end we also display the global trend
-of the corona virus-19 upto date,
-
-<p align = "center">
-   <img src="https://github.com/mv1249/Covid19Tracker/blob/main/images/Fourth.gif" width = "975" height = "475">
-</p>
-
-## Covid-19 Vaccination Dashboard
-
-Here the user has to enter the name of the state for which user is looking for,in the search bar and hit search,the user will be redirected to the Dashboard page,where the user
-can get an idea about the vaccination process which is going around his state,as compared to the more populated states in India,one can get to know the count of dose1,dose2
-and how much % of the crowd is fully vaccinated in the state,and the dashboard gets updated daily,so we get the daily count always!
-
-<p align = "center">
-   <img src="https://github.com/mv1249/Covid19Tracker/blob/main/images/Third.gif" width = "975" height = "475">
-</p>
-
-Explore more on this Dashboard and get all the insights regarding the trends of Covid-19 in India and also regarding Vaccination of Covid-19 in India,here at Covid Tracker😷
-
-## Browse Covid Tracker 😷 here
-
-Official Release : https://trackcovid19here.herokuapp.com/
-
-One can watch the complete preview of Covid Tracker 😷 here : https://github.com/mv1249/Covid19Tracker/issues/1#issue-919445508
-
-## Libraries Used
- 
- <code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/python/python.png"></code>
 <code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"></code>
 <code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"></code>
 <code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"></code>
@@ -84,11 +15,16 @@ One can watch the complete preview of Covid Tracker 😷 here : https://github.c
 <code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png"></code>
 <code><img height="30" src="https://symbols.getvecta.com/stencil_80/56_flask.3a79b5a056.jpg"></code>
 <code><img height="30" src="https://cdn.iconscout.com/icon/free/png-256/heroku-225989.png"></code>
-<code><img height="30" src="https://camo.githubusercontent.com/9be0208aa516b4d1976412d27e9f73d851ea253f8ee005a0b600939f841bba8b/68747470733a2f2f7777772e63686172746a732e6f72672f6d656469612f6c6f676f2d7469746c652e737667"></code>
+
+<code><img height="30" src="https://raw.githubusercontent.com/numpy/numpy/7e7f4adab814b223f7f917369a72757cd28b10cb/branding/icons/numpylogo.svg"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/pandas-dev/pandas/761bceb77d44aa63b71dda43ca46e8fd4b9d7422/web/pandas/static/img/pandas.svg"></code>
+<code><img height="30" src="https://matplotlib.org/_static/logo2.svg"></code>
+<code><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/1280px-Scikit_learn_logo_small.svg.png"></code>
+ <code><img height="30" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/1200px-Tensorflow_logo.svg.png"></code>
+ <code><img height="30" src="https://image.flaticon.com/icons/png/512/873/873120.png"></code>
 
 ## DEPLOYMENT
 
 #### This website is deployed at <code><img height="30" src="https://image.flaticon.com/icons/png/512/873/873120.png"></code>
-#### You can access it [here](https://trackcovid19here.herokuapp.com/)
+#### You can access it [here](https://firstinningsscorepredictor.herokuapp.com/)
 #### Note: The website may take a minute to load sometimes, as the server may be in hibernate state
-
--- app.py
+++ app.py
@@ -1,311 +1,133 @@
 from flask import Flask, render_template, request
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-from collections import defaultdict
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
+model = load_model('iplmodel.h5')
 
 
-@app.route('/')
-def homepage():
-    return render_template('home.html')
+@app.route('/Orangecapdashboard', methods=['GET', 'POST'])
+def orangecapdashboard():
+
+    return render_template('orange.html')
 
 
-@app.route('/dashboard', methods=['GET', 'POST'])
-def hello_world():
+@app.route('/Purplecapdashboard', methods=['GET', 'POST'])
+def purplecapdashboard():
 
-    try:
+    return render_template('purple.html')
 
-        # <----------------------World Data top countries only!------------------------------------------>
 
-        r = requests.get('https://covid19.mathdro.id/api/confirmed')
-        arr = list(r.json())
+@app.route('/', methods=['GET', 'POST'])
+def getdata():
 
-        confirmed_map = defaultdict(list)
-        deaths_map = defaultdict(list)
-        recovered_map = defaultdict(list)
-        for i in range(len(arr)):
-            confirmed_map[arr[i]['countryRegion']].append(arr[i]['confirmed'])
-            recovered_map[arr[i]['countryRegion']].append(arr[i]['recovered'])
-            deaths_map[arr[i]['countryRegion']].append(arr[i]['deaths'])
+    if request.method == 'POST':
+        cap = request.form['cap']
+        # print(cap, season)
+        if cap == 'OrangeCap':
+            return render_template('Orangecapdashboard.html', cap=cap)
 
-        for key, value in confirmed_map.items():
-            confirmed_map[key] = sum(value)
-
-        # US is having "None" type so deleting the key!
-        del recovered_map['US']
-        for key, value in recovered_map.items():
-            recovered_map[key] = sum(value)
-
-        recovered_map['US'] = 0
-
-        for key, value in deaths_map.items():
-            deaths_map[key] = sum(value)
-
-        df = pd.DataFrame()
-        df['Country'] = list(confirmed_map.keys())
-        df['ConfirmedCases'] = list(confirmed_map.values())
-        df['RecoveredCases'] = list(recovered_map.values())
-        df['DeathCases'] = list(deaths_map.values())
-
-        # Extracting the top15 effected countries
-
-        countrylist = list(df['Country'].head(15))
-        globalconfirmedcases = list(df['ConfirmedCases'].head(15))
-        globalrecoveredcases = list(df['RecoveredCases'].head(15))
-        globaldeathcases = list(df['DeathCases'].head(15))
-
-        globalrecoverylist = []
-        for i, j in zip(globalconfirmedcases, globalrecoveredcases):
-            globalrecoverylist.append(round((j/i)*100, 2))
-
-        # print(globalrecoverylist)
-
-        # <--------------------------------India's Data---------------------------------------------------------->
-        r = requests.get(
-            'https://covid19.mathdro.id/api/countries/India/confirmed')
-        arr = list(r.json())
-        datetosend = arr[0]['lastUpdate']
-        stateslist = []
-        confirmed_map = {}
-        deaths_map = {}
-        active_map = {}
-        recovered_map = {}
-        for i in range(len(arr)):
-            stateslist.append(arr[i]['provinceState'].lower())
-            confirmed_map[arr[i]['provinceState'].lower()
-                          ] = arr[i]['confirmed']
-            deaths_map[arr[i]['provinceState'].lower()] = arr[i]['deaths']
-            active_map[arr[i]['provinceState'].lower()] = arr[i]['active']
-            recovered_map[arr[i]['provinceState'].lower()
-                          ] = arr[i]['recovered']
-
-        recoveryrate = []
-        for i, j in zip(list(confirmed_map.values()), list(recovered_map.values())):
-            recoveryrate.append(round((j/i)*100, 2))
-
-        # print(recoveryrate)
-        if request.method == 'POST':
-            state = request.form.get('state')
-            state = [state][0].lower()
-
-            if state in stateslist:
-                confirmed = confirmed_map[state]
-                active = active_map[state]
-                deaths = deaths_map[state]
-                recovered = recovered_map[state]
-                # print(active, confirmed, recovered, deaths)
-                return render_template('index.html', active=active, confirmed=confirmed, deaths=deaths, recovered=recovered, activemap=list(active_map.values()),
-                                       confirmedmap=list(
-                                           confirmed_map.values()),
-                                       recoveredmap=list(
-                                           recovered_map.values()),
-                                       deathsmap=list(deaths_map.values()),
-                                       stateslist=stateslist, datetosend=datetosend, state=[state.capitalize()], showchart=True, recoveryrate=recoveryrate, isrecovery=True, countrylist=countrylist,
-                                       globalconfirmedcases=globalconfirmedcases,
-                                       globalrecoveredcases=globalrecoveredcases,
-                                       globaldeathcases=globaldeathcases,
-                                       globalrecoverylist=globalrecoverylist, showcards=True)
-
-            else:
-                # send an alert to please will the name clearly!
-                return render_template('dashboarderror.html', statenotpresent=True)
-    except:
-        return render_template('exception.html')
+        elif cap == 'PurpleCap':
+            return render_template('Purplecapdashboard.html', cap=cap)
 
     return render_template('index.html')
 
 
-@app.route('/vaccine', methods=['GET', 'POST'])
-def vaccine():
+# <!-- consistent_teams = ['Kolkata Knight Riders', 'Chennai Super Kings', 'Rajasthan Royals',
+#                     'Mumbai Indians', 'Kings XI Punjab', 'Royal Challengers Bangalore',
+#                     'Delhi Daredevils', 'Sunrisers Hyderabad'] -->
+
+@app.route('/predict', methods=['GET', 'POST'])
+def hello_world():
 
     try:
-        url = 'https://en.wikipedia.org/wiki/COVID-19_vaccination_in_India#Andhra_Pradesh'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        data = []
-        table = soup.find('table', attrs={"class": "plainrowheaders"})
-        table_body = table.find('tbody')
 
-        rows = table_body.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            # Get rid of empty values
-            data.append([ele for ele in cols if ele])
+        team_dict = {1: 'Kolkata Knight Riders', 2: 'Chennai Super Kings', 3: 'Rajasthan Royals', 4: 'Mumbai Indians',
+                     5: 'Kings XI Punjab', 6: 'Royal Challengers Bangalore', 7: 'Delhi Daredevils', 8: 'Sunrisers Hyderabad'}
 
-        data = data[2:]
-        data = data[:-2]
-        states = ['andaman and nicobar islands', 'andhra pradesh', 'arunachal pradesh', 'assam', 'bihar', 'chandigarh', 'chhattisgarh', 'dadra and nagar haveli', 'daman and diu', 'delhi', 'goa', 'gujarat', 'haryana', 'himachal pradesh', 'jammu and kashmir', 'jharkhand',
-                  'karnataka', 'kerala', 'ladakh', 'lakshadweep', 'madhya pradesh', 'maharashtra', 'manipur', 'meghalaya', 'mizoram', 'nagaland', 'odisha', 'puducherry', 'punjab', 'rajasthan', 'sikkim', 'tamil nadu', 'telangana', 'tripura', 'uttar pradesh', 'uttarakhand', 'west bengal']
-
-        population = [int(''.join(data[i][0].split(',')))
-                      for i in range(len(data))]
-        dose1 = [int(''.join(data[i][1].split(','))) for i in range(len(data))]
-        dose2 = [int(''.join(data[i][2].split(','))) for i in range(len(data))]
-        atmostonedose = [float(data[i][4].split('%')[0])
-                         for i in range(len(data))]
-        fullyvaccinated = [float(data[i][5].split('%')[0])
-                           for i in range(len(data))]
-
-        # creating maps
-        populationmap = dict(list(
-            (sorted(dict(zip(states, population)).items(), key=lambda x: x[1], reverse=True))))
-        dose1_map = dict(list(
-            (sorted(dict(zip(states, dose1)).items(), key=lambda x: x[1], reverse=True))))
-        dose2_map = dict(list(
-            (sorted(dict(zip(states, dose2)).items(), key=lambda x: x[1], reverse=True))))
-        atmostonedose_map = dict(list((sorted(
-            dict(zip(states, atmostonedose)).items(), key=lambda x: x[1], reverse=True))))
-        fulldosemap = dict(list((sorted(
-            dict(zip(states, fullyvaccinated)).items(), key=lambda x: x[1], reverse=True))))
-
-        # segregating the top doses,top doses will give a value if compared to states which are having more pouplation instead of less,so i have segrated the dose results based on the highly populated states!
-
-        def getnewmap(topstates, mapper):
-            maps = {}
-            for key, val in mapper.items():
-                if key in list(topstates.keys()):
-                    maps[key] = val
-
-            return maps
-
-        # segregating the top doses
-
-        topstates = dict(list(populationmap.items())[:5])
-        topdose1 = getnewmap(topstates, dose1_map)
-        topdose2 = getnewmap(topstates, dose2_map)
-        toponedose = getnewmap(topstates, atmostonedose_map)
-        topfullvaccine = getnewmap(topstates, fulldosemap)
-
-        # retriving the data from the form
+        final_array = []
+        temp_array = []
+        batting_team = [0]*8
+        bowling_team = [0]*8
+        wickets_array = [0]*10
+        overs_array = [0]*10
 
         if request.method == 'POST':
 
-            try:
-                statename = request.form.get('state').lower()
-                dose1 = dose1_map[statename]
-                dose2 = dose2_map[statename]
-                atmostonedose = atmostonedose_map[statename]
-                fulldose = fulldosemap[statename]
-                populationstate = populationmap[statename]
+            # Batting team
+            team_bat = request.form['batting']
+            team_bowl = request.form['bowling']
+            current_wicket = request.form['currentwickets']
+            current_overs = request.form['currentovers']
+            current_score = int(request.form['currentruns'])
 
-                print(dose1, dose2, atmostonedose, fulldose)
-                return render_template('vaccine.html', showcards=True, dose1=dose1, dose2=dose2, atmostonedose=atmostonedose, fulldose=fulldose, state=[statename.capitalize()],
-                                       topstates=list(topstates.keys()), population=list(topstates.values()),
-                                       statedose1=list(topdose1.values()), statedose2=list(topdose2.values()),
-                                       toponedose=list(toponedose.values()), tofulldose=list(topfullvaccine.values()), populationstate=populationstate,
-                                       showchart=True, fullyvaccinatedstate=list(fulldosemap.keys()),
-                                       fullyvaccinatedvalues=list(
-                    fulldosemap.values()),
-                    atmostonedosemapstate=list(
-                    atmostonedose_map.keys()),
-                    atmostonedosevalue=list(atmostonedose_map.values()),)
-            except:
-                return render_template('vaccineerror.html', statenotpresent=True)
-    except:
-        return render_template('exception.html')
+            if team_bat == team_bowl:
+                return render_template('error.html')
 
-    return render_template('vaccine.html')
+            else:
 
+                # Batting team
+                for key, val in team_dict.items():
+                    if team_bat == val:
+                        batting_team[key-1] = 1
 
-@app.route('/covidtable', methods=['GET', 'POST'])
-def gettable():
+                # Bowling team
 
-    try:
-        r = requests.get(
-            'https://covid19.mathdro.id/api/countries/India/confirmed')
-        arr = list(r.json())
-        datetosend = arr[0]['lastUpdate']
-        stateslist = []
-        confirmed_map = {}
-        deaths_map = {}
-        active_map = {}
-        recovered_map = {}
-        for i in range(len(arr)):
-            stateslist.append(arr[i]['provinceState'])
-            confirmed_map[arr[i]['provinceState']] = arr[i]['confirmed']
-            deaths_map[arr[i]['provinceState']] = arr[i]['deaths']
-            active_map[arr[i]['provinceState']] = arr[i]['active']
-            recovered_map[arr[i]['provinceState']] = arr[i]['recovered']
+                for key, val in team_dict.items():
+                    if team_bowl == val:
+                        bowling_team[key-1] = 1
 
-        active = list(active_map.values())
-        confirmed = list(confirmed_map.values())
-        deaths = list(deaths_map.values())
-        recovered = list(recovered_map.values())
+                # Wickets array
 
-        # recoveryrate = recovered/confirmed*100
+                for i in range(1, len(wickets_array)):
+                    if int(current_wicket) == i:
+                        wickets_array[i-1] = 1
 
-        recoveryrate = [round(((i/j)*100), 2)
-                        for i, j in zip(recovered, confirmed)]
+                # print(f' Wickets array : {wickets_array}')
 
-        alltogether = []
-        for state, i, j, k, l, m in zip(stateslist, active, confirmed, deaths, recovered, recoveryrate):
-            alltogether.append([state, i, j, k, l, m])
+                # Current overs
 
-        return render_template('covidtable.html', active=sum(active),
-                               confirmed=sum(confirmed), deaths=sum(deaths), recovered=sum(recovered),
-                               date=datetosend, details=alltogether)
+                for i in range(1, len(overs_array)):
+                    if int(current_overs) == i:
+                        overs_array[i-1] = 1
 
-    except:
-        return render_template('exception.html')
+                final_array = final_array+batting_team+bowling_team+wickets_array+overs_array
+                final_array.append(current_score)
 
-    return render_template('home.html')
+                mm = MinMaxScaler()
+                final_array = np.array([final_array])
+                # print(final_array)
 
+                # trained_scaled = mm.fit_transform(final_array)
+                # print(trained_scaled)
 
-@app.route('/vaccinetable', methods=['GET', 'POST'])
-def getvaccinetable():
+                predictions = model.predict(final_array)
+                value = int(int(predictions[0][0])//100)
 
-    try:
+                if current_score > 100 and int(current_wicket) <= 5:
+                    value = value-50
+                    return render_template('result.html', value=value)
 
-        # Scrapping Date
+                elif current_score > 100 and int(current_wicket) >= 5 and int(current_overs) >= 10:
+                    value = value-75
+                    return render_template('result.html', value=value)
 
-        r = requests.get(
-            'https://covid19.mathdro.id/api/countries/India/confirmed')
-        arr = list(r.json())
-        datetosend = arr[0]['lastUpdate']
+                elif int(current_wicket) == 9 or int(current_wicket) == 8:
+                    value = current_score+10
+                    return render_template('result.html', value=value)
 
-        url = 'https://en.wikipedia.org/wiki/COVID-19_vaccination_in_India#Andhra_Pradesh'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        data = []
-        table = soup.find('table', attrs={"class": "plainrowheaders"})
-        table_body = table.find('tbody')
+                else:
+                    return render_template('result.html', value=value)
 
-        rows = table_body.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            # Get rid of empty values
-            data.append([ele for ele in cols if ele])
+                # print(value)
 
-        data = data[2:]
-        data = data[:-2]
-        states = ['andaman and nicobar islands', 'andhra pradesh', 'arunachal pradesh', 'assam', 'bihar', 'chandigarh', 'chhattisgarh', 'dadra and nagar haveli', 'daman and diu', 'delhi', 'goa', 'gujarat', 'haryana', 'himachal pradesh', 'jammu and kashmir', 'jharkhand',
-                  'karnataka', 'kerala', 'ladakh', 'lakshadweep', 'madhya pradesh', 'maharashtra', 'manipur', 'meghalaya', 'mizoram', 'nagaland', 'odisha', 'puducherry', 'punjab', 'rajasthan', 'sikkim', 'tamil nadu', 'telangana', 'tripura', 'uttar pradesh', 'uttarakhand', 'west bengal']
-        population = [int(''.join(data[i][0].split(',')))
-                      for i in range(len(data))]
-        dose1 = [int(''.join(data[i][1].split(','))) for i in range(len(data))]
-        dose2 = [int(''.join(data[i][2].split(','))) for i in range(len(data))]
-        atmostonedose = [float(data[i][4].split('%')[0])
-                         for i in range(len(data))]
-        fullyvaccinated = [float(data[i][5].split('%')[0])
-                           for i in range(len(data))]
-
-        alltogether = []
-        for i, j, k, l, m, n in zip(states, population, dose1, dose2, atmostonedose, fullyvaccinated):
-            alltogether.append([i, j, k, l, m, n])
-
-        # print(alltogether)
-        return render_template('vaccinetable.html', date=datetosend,
-                               totalpopulation=sum(population), totaldose1=sum(dose1), totaldose2=sum(dose2), totaldose=round(sum(fullyvaccinated)/len(fullyvaccinated), 2),
-                               alltogether=alltogether)
+        return render_template('predict.html')
 
     except:
-        return render_template('exception.html')
-
-    return render_template('vaccinetable.html')
+        return render_template('error.html')
 
 
 if __name__ == '__main__':
